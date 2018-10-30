86c118b53fc354492f7bb2ed0f9affeef545abc3
Dmitriy Petrishin <dpe@primexm.com>
2018-10-30 23:05:41 +0400

    Combine all written logic in program.main.

--- FontAwesomeParser.Terminal/Program.cs
+++ FontAwesomeParser.Terminal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace FontAwesomeParser.Terminal
 {
@@ -6,7 +8,22 @@ namespace FontAwesomeParser.Terminal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileName = "all.css";
+
+            var currentPath = Directory.GetCurrentDirectory();
+            var fileFullPath = Path.Combine(currentPath, fileName);
+            
+            Console.WriteLine($"Put the all.css file in {currentPath}");
+            Console.ReadKey();
+            
+            var content = DirectoryHelper.ReadFromFile(fileFullPath);
+            var cssParser = new CssParser(content);
+            cssParser.Parse();
+            
+            var enumGen = new TsEnumGenerator(cssParser.Result);
+            var result = enumGen.Generate();
+            
+            File.AppendAllText(Path.Combine(currentPath, "index.ts"), result.ToString());
         }
     }
 }
